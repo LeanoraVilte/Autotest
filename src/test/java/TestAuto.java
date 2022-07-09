7a228c8d1ff6e6284ca2f486b48b2c48e347493f
LeanoraVilte <tina.ww.lea@gmail.com>
2022-07-09 22:04:32 +0300

    re8

--- src/test/java/TestAuto.java
+++ src/test/java/TestAuto.java
@@ -1,12 +1,9 @@
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public abstract class Testauto {
-
-    abstract executeJavaScript("$('footer').remove()");
+public class TestAuto {
 
     @Test
     void successfulSearchTest() {
@@ -25,17 +22,16 @@ public abstract class Testauto {
     @Test
     void DemoQatest()
     {
-
         open("https://demoqa.com/automation-practice-form");
         $("[id = firstName]").setValue("Kristina");
         $("[id = lastName]").setValue("Vosk");
         $("[id = userEmail]").setValue("tina@mail.ru");
         $("[id = gender-radio-2]").doubleClick();
         $("[id = userNumber]").setValue("1234567890");
-        $("[id = dateOfBirthInput]").setValue("18 May 1988");
-
-        sleep(500);
-
+        $("[id = currentAddress]").setValue("New town, 44");
+        //$("[id = dateOfBirthInput]").setValue("18 May 1988");
+        //$("[id = hobbies-checkbox-2]").doubleClick();
+        sleep(6000);
 
     }
 }
