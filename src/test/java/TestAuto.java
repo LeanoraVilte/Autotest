c7a6a11879d22386ac4b6babba1bfc2ae9c8c82a
LeanoraVilte <tina.ww.lea@gmail.com>
2022-07-10 11:30:37 +0300

    re11

--- src/test/java/TestAuto.java
+++ src/test/java/TestAuto.java
@@ -1,9 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class TestAuto {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
 
     @Test
     void successfulSearchTest() {
@@ -22,14 +34,34 @@ public class TestAuto {
     @Test
     void DemoQatest()
     {
-        open("https://demoqa.com/automation-practice-form");
+
+        String hobby = "Reading";
+        String city = "Belarus";
+        String state = "Bel";
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
         $("[id = firstName]").setValue("Kristina");
         $("[id = lastName]").setValue("Vosk");
         $("[id = userEmail]").setValue("tina@mail.ru");
         $("[id = gender-radio-2]").doubleClick();
         $("[id = userNumber]").setValue("1234567890");
         $("[id = currentAddress]").setValue("New town, 44");
-        //$("[id = dateOfBirthInput]").setValue("18 May 1988");
+        $("[id = dateOfBirthInput]").clear();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("1988");
+        $(".react-datepicker__day--018:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Games").pressEnter();
+        $("#hobbiesWrapper").setValue("Reading").click();
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
+        // $("input#uploadPicture").uploadFile(new File("src/test/resources/123.jpg"));
+        $("input#uploadPicture").uploadFromClasspath("123.jpg");
+        //  $("#uploadPicture").uploadFromClasspath(imgPath);  альтернатива,
+        //  задать переменную String imgPath = "img/Pushkin.jpg";
+        $("#submit").click();
         //$("[id = hobbies-checkbox-2]").doubleClick();
         sleep(6000);
 
